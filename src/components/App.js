8b2bac47cd77107d0ff861363fbde0cb298d8be9
Gee <glynester@hotmail.com>
2022-03-03 19:29:16 +0000

    Refactor App to class. Pass search term from component to App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App=()=>{
-  return (
+class App extends React.Component {
+// callback to be passed to SearchBar component
+  onSearchSubmit(term){
+    console.log(term);
+  }
+
+  // onSubmit below could have been named something else
+  render(){ 
+    return (
     <div className="ui container" style={{ marginTop: '10px'}}>
-      <SearchBar />
+      <SearchBar onSubmit={this.onSearchSubmit}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,28 +7,17 @@ import React from 'react';
 class SearchBar extends React.Component{
   state = { term: '' };
 
-  // Makes a new function with the correct value of "this" - overwrites old function.
-  // constructor(){
-  //   super();
-  //  this.onFormSubmit=this.onFormSubmit.bind(this); 
-  // }
-
-
-  // Incorrect value of "this" if callback is not an arrow function
-  // onFormSubmit(event){
-  //   event.preventDefault();
-  //   console.log(this.state.term)
-  // }
-
   onFormSubmit=(event)=>{
     event.preventDefault();
-    console.log(this.state.term)
+    console.log(this.props.onSubmit); // function onSearchSubmit(term)
+      // We are in a class based component so we reference the props obj with "this.props.onSubmit" - THIS
+    // onSubmit was passed in as a prop from App component  
+    this.props.onSubmit(this.state.term);
   }
 
   render(){
     return (
     <div className="ui segment">
-      {/* {<form onSubmit={(event)=>this.onFormSubmit(event)}  */}
       <form onSubmit={this.onFormSubmit} className="ui form">
         <div className="field">
           <label>Image Search</label>
