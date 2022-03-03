0109e6aa4c406f6b85e55b1dd094a3051779ed62
Gee <glynester@hotmail.com>
2022-03-03 20:56:46 +0000

    Installed axios to fetch data from unsplash API and set up API call.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 // callback to be passed to SearchBar component
   onSearchSubmit(term){
     console.log(term);
+    axios.get('https://api.unsplash.com/search/photos',{
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID u0pvA-mBrIpcFo9Z1TfIPlzz_CRTOEmWrIwbDaAykKk'
+      }
+    });
   }
 
   // onSubmit below could have been named something else
